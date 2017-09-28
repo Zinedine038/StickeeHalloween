989357c0bcd770c7376be6e8e841d67bbf83a1ab
zinedine038 <zsghari@hotmail.com>
2017-09-28 10:03:59 +0100

    Zombies again

--- Stickee - Halloween 2017/Assets/ZombieMotor.cs
+++ Stickee - Halloween 2017/Assets/ZombieMotor.cs
@@ -4,15 +4,28 @@ using UnityEngine.AI;
 using UnityEngine;
 
 public class ZombieMotor : MonoBehaviour {
+    public Animator animator;
     public NavMeshAgent nav;
     public GameObject player;
+    public float speed;
 	// Use this for initialization
 	void Start () {
 		nav.SetDestination(player.transform.position);
+        nav.speed=speed;    
+        animator.SetFloat("Speed",speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator IncreaseMotorSpeed(float newSpeed)
+    {
+        while(animator.GetFloat("Speed")!=newSpeed)
+        {
+            animator.SetFloat("Speed",Mathf.Lerp(animator.GetFloat("Speed"),newSpeed,1*Time.deltaTime));
+            yield return null;
+        }
+    }
 }
