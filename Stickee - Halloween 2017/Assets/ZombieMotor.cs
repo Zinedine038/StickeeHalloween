dacde6340203f244e127e0a35bd4f8484aaa1382
zinedine038 <zsghari@hotmail.com>
2017-09-28 10:45:20 +0100

    AI Progress

--- Stickee - Halloween 2017/Assets/ZombieMotor.cs
+++ Stickee - Halloween 2017/Assets/ZombieMotor.cs
@@ -8,8 +8,10 @@ public class ZombieMotor : MonoBehaviour {
     public NavMeshAgent nav;
     public GameObject player;
     public float speed;
-	// Use this for initialization
-	void Start () {
+    private bool mayAttack = true;
+
+    // Use this for initialization
+    void Start () {
 		nav.SetDestination(player.transform.position);
         nav.speed=speed;    
         animator.SetFloat("Speed",speed);
@@ -17,7 +19,10 @@ public class ZombieMotor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Vector3.Distance(transform.position,player.transform.position)<=2)
+        {
+            Attack();
+        }
 	}
 
     IEnumerator IncreaseMotorSpeed(float newSpeed)
@@ -27,5 +32,22 @@ public class ZombieMotor : MonoBehaviour {
             animator.SetFloat("Speed",Mathf.Lerp(animator.GetFloat("Speed"),newSpeed,1*Time.deltaTime));
             yield return null;
         }
+    }
+
+    void Attack()
+    {
+        if(mayAttack)
+        {
+            StartCoroutine("AttackPlayer");
+        }
+    }
+
+    private IEnumerator AttackPlayer()
+    {
+        mayAttack=false;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(1f);
+        //hpdown
+        mayAttack=true;
     }
 }
